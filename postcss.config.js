c374c7624c5c80bbb47ec22c4d877c650b6a4226
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-11-14 15:36:31 +0100

    Feat: add postcss-gap-properties plugin, and update postcss t latest version

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer')({ grid: "no-autoplace" }),
+    require('postcss-gap-properties'),
     require('postcss-focus'),
     require('postcss-object-fit-images'),
     require('cssnano')({
