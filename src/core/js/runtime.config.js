acc468aaaf24a98800732945abccfc3862f6f6b4
SeptimusVII <vansteene.quentin@gmail.com>
2025-10-23 18:07:34 +0200

    remove tests lines from runtime config files

--- src/core/js/runtime.config.js
+++ src/core/js/runtime.config.js
@@ -1,3 +1,1 @@
 /* /!\ WARNING: this file is rewritten at compilation. Do not edit unless you know what you're doing. */
-require('./../../components/testComponent/testComponent.js');
-require('./../../themes/testTheme/testTheme.js');
