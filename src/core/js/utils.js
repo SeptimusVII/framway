0b7d6af7e08e6b4f861ecfdd5983d94773f7fd07
SeptimusVII <vansteene.quentin@gmail.com>
2026-02-18 16:01:20 +0100

    utils: add isWithinViewport function

--- src/core/js/utils.js
+++ src/core/js/utils.js
@@ -583,6 +583,15 @@ var Utils = function Utils(){
   };
 
   utils.isWithinViewport = function(el, partiallyVisible = false) {
+    const { top, left, bottom, right } = el.getBoundingClientRect();
+    const { innerHeight, innerWidth } = window;
+    return partiallyVisible
+      ? ((top > 0 && top < innerHeight) ||
+          (bottom > 0 && bottom < innerHeight)) &&
+          ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+      : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+  };
+
   
   // --> FORM VALIDATION
   
