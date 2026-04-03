75d321215404b7509885196d5ba6720bce3696cc
SeptimusVII <vansteene.quentin@gmail.com>
2026-04-03 09:32:56 +0200

    update framway's config  (not definitive)

--- framway.config.js
+++ framway.config.js
@@ -1,6 +1,34 @@
 module.exports = {
     "themes": [
+        "testTheme",
+        // "testTheme2"
     ],
     "components": [
+        // "accordionFW",
+        "block-std",
+        // "block-card",
+        // "block-editable",
+        // "btn-load",
+        // "countUpFW",
+        // "datepicker",
+        // "fileUploader",
+        // "filters",
+        // "goto",
+        // "headerFW",
+        // "heroFW",
+        // "input-number",
+        // "input-range",
+        // "ipsum",
+        // "modalFW",
+        // "pricecard",
+        // "select2FW",
+        // "sidepanel",
+        // "sliderFW",
+        // "splitForm",
+        "table-list",
+        "tabs",
+        // "text-editor",
+        // "wizard"
     ],
+    "debug": true
 }
